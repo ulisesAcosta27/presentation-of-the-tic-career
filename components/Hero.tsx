dd590dbe8b11e885826cec60aa17426677662c69
Ulises David Acosta <ulisesdavidacosta27@gmail.com>
2023-10-27 21:39:58 -0300

    feature: transition example

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+"use client";
 import Image from "next/image";
 
+import { motion } from "framer-motion";
+
 import Photo1 from "@/static/img/Foto-1.png";
+
+const defaulAnimation = {
+  hidden: {
+    opacity: 0,
+    y: -20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 export const Hero = () => {
   return (
@@ -10,24 +23,48 @@ export const Hero = () => {
         <div className="w-full h-full grid grid-cols-1 md:grid-cols-2">
           <div className="flex items-center justify-center">
             <div className="text-center px-4 md:text-start">
-              <h2 className="font-semibold text-2xl xl:text-3xl">
+              <motion.h2
+                initial="hidden"
+                animate="visible"
+                variants={defaulAnimation}
+                className="font-semibold text-2xl xl:text-3xl"
+              >
                 Las TICs Son Esenciales
-              </h2>
-              <p className="text-sm w-full lg:text-base">
+              </motion.h2>
+              <motion.p
+                className="text-sm w-full lg:text-base"
+                initial="hidden"
+                animate="visible"
+                transition={{ delay: 0.1 }}
+                variants={defaulAnimation}
+              >
                 Transforma la educación, inspira el futuro. Forjando innovadores
                 en TICs para una enseñanza conectada
-              </p>
-              <div className="my-2 bg-black text-white py-2 px-8 inline-block rounded-full hover:bg-white border border-black hover:text-black ease-in-out duration-300">
-                <p className="text-sm">Ver Mas</p>
-              </div>
+              </motion.p>
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={defaulAnimation}
+                transition={{ type: "tween", delay: 0.2 }}
+              >
+                <div className="my-2 bg-black text-white py-2 px-8 inline-block rounded-full hover:bg-white border border-black hover:text-black ease-in-out duration-300">
+                  <p className="text-sm">Ver Mas</p>
+                </div>
+              </motion.div>
             </div>
           </div>
           <div className="hidden md:flex items-center justify-center">
-            <Image
-              className="md:w-[380px] lg:w-[420px] xl:w-[480px]"
-              src={Photo1}
-              alt="Personas bucando iformacion de las TICs"
-            />
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={defaulAnimation}
+            >
+              <Image
+                className="md:w-[380px] lg:w-[420px] xl:w-[480px]"
+                src={Photo1}
+                alt="Personas bucando iformacion de las TICs"
+              />
+            </motion.div>
           </div>
         </div>
       </div>
